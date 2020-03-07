0654cdf04264033dce463c69149d813cebe98042
Andrea Borruso <aborruso@gmail.com>
2020-03-07 21:08:12 +0100

    inserito data nei nomi output

--- webservices/webMapServices.sh
+++ webservices/webMapServices.sh
@@ -5,6 +5,7 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 set -x
 
 while IFS=$'\t' read -r url layer; do
-    curl -sL "$url" >"$folder"/webMapServices/"$layer".geojson
-    ogr2ogr -f CSV -lco GEOMETRY=AS_XY "$folder"/webMapServices/"$layer".csv "$folder"/webMapServices/"$layer".geojson
+    iso=$(date --iso-8601)
+    curl -sL "$url" >"$folder"/webMapServices/"$iso"_"$layer".geojson
+    ogr2ogr -f CSV -lco GEOMETRY=AS_XY "$folder"/webMapServices/"$iso"_"$layer".csv "$folder"/webMapServices/"$iso"_"$layer".geojson
 done <"$folder"/webMapServicesList.tsv
