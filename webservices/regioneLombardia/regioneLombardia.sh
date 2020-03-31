6b5f3806664afe94ea47644407849dffc0678fb7
aborruso <aborruso@gmail.com>
2020-03-31 13:16:01 +0200

    update

--- webservices/regioneLombardia/regioneLombardia.sh
+++ webservices/regioneLombardia/regioneLombardia.sh
@@ -26,6 +26,12 @@ ogr2ogr -f CSV "$folder"/processing/PROVINCE_COVID19.csv "$folder"/rawdata/PROVI
 
 <"$folder"/rawdata/INCR_DATE_PRV_TAMP_RL_v2.json jq '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_PRV_TAMP_RL_v2.csv
 
+mkdir -p "$folder"/processing/INCR_DATE_PRV_TAMP_RL_v2
+
+date=$(date '+%Y-%m-%d')
+
+<"$folder"/rawdata/INCR_DATE_PRV_TAMP_RL_v2.json jq '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_PRV_TAMP_RL_v2/"$date"-INCR_DATE_PRV_TAMP_RL_v2.csv
+
 # commit e push
 
 . ~/.keychain/$HOSTNAME-sh
