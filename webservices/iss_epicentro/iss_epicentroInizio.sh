295f219b76e8e4b8a6cfba750a762118de218bd7
aborruso <aborruso@gmail.com>
2020-06-27 00:14:56 +0200

    prima versione estrazione dati dashboard inizio

---
+++ webservices/iss_epicentro/iss_epicentroInizio.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$folder"/rawdata
+mkdir -p "$folder"/processing
+mkdir -p "$folder"/risorse
+
+# svuota cartella
+#rm "$folder"/processing/*
+rm "$folder"/rawdata/*
+
+### dashboard inizio ###
+urlinizio="https://www.epicentro.iss.it/coronavirus/dashboard/inizio.html"
+
+# estrai id dei div html
+curl -kL "$urlinizio" >"$folder"/rawdata/Dashboard_finale_dallinizio.html
+scrape <"$folder"/rawdata/Dashboard_finale_dallinizio.html -be '//div[contains(@id,"htmlwidget-")]' |
+  xq -r '.html.body.div[]."@id"' >"$folder"/rawdata/listaDivIdDallInizio
+
+# per ogni div estrai i dati JSON
+while read p; do
+  echo "$p"
+  scrape <"$folder"/rawdata/Dashboard_finale_dallinizio.html -be '//div[@id="'"$p"'"]/following::script[1]' |
+    xq -r '.html.body.script."#text"' | jq . >"$folder"/rawdata/"$p".json
+done <"$folder"/rawdata/listaDivIdDallInizio
+
+# Incidenza di COVID-19 segnalati in Italia per Regione/Provincia Autonoma per data di prelievo o diagnosi
+
+numeroClassi=$(expr $(jq <"$folder"/rawdata/htmlwidget-0971c37dd630e105e8c2.json '.x.data| length') - 1)
+
+rm "$folder"/tmp_classe_*.csv
+for i in $(seq 0 "$numeroClassi"); do
+  jq <"$folder"/rawdata/htmlwidget-0971c37dd630e105e8c2.json '.x.data['"$i"']' | mlr --j2c reshape -r "^[xy]:" -o item,value then cut -f item,value then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_1,value then put '$classe='"$i"'' then rename item_2,id >"$folder"/rawdata/tmp_classe_"$i".csv
+  numerorighe=$(cat "$folder"/tmp_classe_"$i".csv | tail -n +2 | wc -l)
+  mlr --ocsv seqgen --step 6 --start 0 --stop "$numerorighe" -f id then put '$code=$id' >>"$folder"/rawdata/tmp_classe_"$i"_join.csv
+  mlr --csv join --ul -j id -f "$folder"/tmp_classe_"$i".csv then unsparsify then sort -n id then fill-down -f code then cat -n -g code "$folder"/tmp_classe_"$i"_join.csv >"$folder"/rawdata/tmp_classe_"$i"_out.csv
+done
+
+mlr --csv cat "$folder"/rawdata/tmp_classe_*_out.csv >"$folder"/processing/incidenzaInizio.csv
+
+#  jq <rawdata/htmlwidget-0971c37dd630e105e8c2.json '.x.data['"$i"']' | mlr --j2c reshape -r "^[xy]:" -o item,value then cut -f item,value then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_1,value then put '$classe='"$i"'' >tmp_classe_"$i".csv
+#jq <rawdata/htmlwidget-0971c37dd630e105e8c2.json '.x.data['"$i"']' | mlr --j2c reshape -r "^[xy]:" -o item,value then cut -f item,value then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_1,value then put '$classe='"$i"'' >tmp_classe_"$i".csv
+
+# mlr --csv join --ul -j id -f esempioa.csv then unsparsify then sort -n id then fill-down -f code then cat -n -g code join.csv
+
+#python3 "$folder"/script_v2.py
