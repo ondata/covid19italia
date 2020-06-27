088adb527798cacfb88ba2486b869a41843cc9fd
Andrea Borruso <aborruso@gmail.com>
2020-06-27 13:06:21 +0200

    estrazione dati grezzi heatmap

---
+++ webservices/iss_epicentro/iss_epicentroInizio.py
@@ -0,0 +1,40 @@
+import csv
+import datetime
+
+regioni = ['Piemonte', 'Valle Aosta', 'Lombardia', 'PA Bolzano', 'PA Trento', 'Veneto', 'Friuli', 'Liguria', 'Emilia', 'Toscana',
+           'Umbria', 'Marche', 'Lazio', 'Abruzzo', 'Molise', 'Campania', 'Puglia', 'Basilicata', 'Calabria', 'Sicilia', 'Sardegna']
+
+classi_casi = ['1-10', '11-50', '51-100',
+               '101-200', '201-500', '501-1000', '1000+']
+
+classi_inc = ['0.01-1', '1.01-5', '5.01-10',
+              '10.01-15', '15.01-20', '20.01-40', '40+']
+
+first_day = "20-02-19"
+date_start = datetime.datetime.strptime(first_day, "%y-%m-%d")
+
+f2 = open('./processing/incidenzaInizio.csv', 'w')
+f2.write('data,regione,classe_incidenza\n')
+
+with open('./rawdata/incidenzaInizio.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Intestazione {", ".join(row)}')
+            line_count += 1
+        elif row[0] == '1' and int(row[4]) < 7:
+            day = (int(row[2])-1582113600)/86400
+            id_regione = int((float(row[3])-0.5))
+            my_classe = int(row[4])
+            my_date = date_start + datetime.timedelta(days=day)
+            print(
+                f'\t{my_date.day}/{my_date.month} \t {regioni[id_regione]} \t {classi_inc[my_classe]}')
+            f2.write('{:%y-%m-%d}'.format(my_date)+',' +
+                     regioni[id_regione]+','+classi_inc[my_classe]+'\n')
+            line_count += 1
+    print(f'Elaborate {line_count} righe.')
+
+
+f2.close()
+
--- webservices/iss_epicentro/iss_epicentroInizio.sh
+++ webservices/iss_epicentro/iss_epicentroInizio.sh
@@ -4,6 +4,8 @@ set -x
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+date=$(date '+%Y-%m-%d')
+
 mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/processing
 mkdir -p "$folder"/risorse
@@ -34,16 +36,41 @@ numeroClassi=$(expr $(jq <"$folder"/rawdata/htmlwidget-0971c37dd630e105e8c2.json
 rm "$folder"/tmp_classe_*.csv
 for i in $(seq 0 "$numeroClassi"); do
   jq <"$folder"/rawdata/htmlwidget-0971c37dd630e105e8c2.json '.x.data['"$i"']' | mlr --j2c reshape -r "^[xy]:" -o item,value then cut -f item,value then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_1,value then put '$classe='"$i"'' then rename item_2,id >"$folder"/rawdata/tmp_classe_"$i".csv
-  numerorighe=$(cat "$folder"/tmp_classe_"$i".csv | tail -n +2 | wc -l)
+  numerorighe=$(cat "$folder"/rawdata/tmp_classe_"$i".csv | tail -n +2 | wc -l)
   mlr --ocsv seqgen --step 6 --start 0 --stop "$numerorighe" -f id then put '$code=$id' >>"$folder"/rawdata/tmp_classe_"$i"_join.csv
-  mlr --csv join --ul -j id -f "$folder"/tmp_classe_"$i".csv then unsparsify then sort -n id then fill-down -f code then cat -n -g code "$folder"/tmp_classe_"$i"_join.csv >"$folder"/rawdata/tmp_classe_"$i"_out.csv
+  mlr --csv join --ul -j id -f "$folder"/rawdata/tmp_classe_"$i".csv then unsparsify then sort -n id then fill-down -f code then cat -n -g code "$folder"/rawdata/tmp_classe_"$i"_join.csv >"$folder"/rawdata/tmp_classe_"$i"_out.csv
 done
 
-mlr --csv cat "$folder"/rawdata/tmp_classe_*_out.csv >"$folder"/processing/incidenzaInizio.csv
+mlr --csv cat "$folder"/rawdata/tmp_classe_*_out.csv >"$folder"/rawdata/incidenzaInizio.csv
 
-#  jq <rawdata/htmlwidget-0971c37dd630e105e8c2.json '.x.data['"$i"']' | mlr --j2c reshape -r "^[xy]:" -o item,value then cut -f item,value then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_1,value then put '$classe='"$i"'' >tmp_classe_"$i".csv
-#jq <rawdata/htmlwidget-0971c37dd630e105e8c2.json '.x.data['"$i"']' | mlr --j2c reshape -r "^[xy]:" -o item,value then cut -f item,value then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_1,value then put '$classe='"$i"'' >tmp_classe_"$i".csv
+# Numero di casi di COVID-19 segnalati in Italia per Regione/Provincia Autonoma per data di prelievo o diagnosi
 
-# mlr --csv join --ul -j id -f esempioa.csv then unsparsify then sort -n id then fill-down -f code then cat -n -g code join.csv
+numeroClassi=$(expr $(jq <"$folder"/rawdata/htmlwidget-0f53bc84789d52b2599d.json '.x.data| length') - 1)
 
-#python3 "$folder"/script_v2.py
+rm "$folder"/tmp_classe_*.csv
+for i in $(seq 0 "$numeroClassi"); do
+  jq <"$folder"/rawdata/htmlwidget-0f53bc84789d52b2599d.json '.x.data['"$i"']' | mlr --j2c reshape -r "^[xy]:" -o item,value then cut -f item,value then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_1,value then put '$classe='"$i"'' then rename item_2,id >"$folder"/rawdata/tmp_classe_nc_"$i".csv
+  numerorighe=$(cat "$folder"/rawdata/tmp_classe_nc_"$i".csv | tail -n +2 | wc -l)
+  mlr --ocsv seqgen --step 6 --start 0 --stop "$numerorighe" -f id then put '$code=$id' >>"$folder"/rawdata/tmp_classe_nc_"$i"_join.csv
+  mlr --csv join --ul -j id -f "$folder"/rawdata/tmp_classe_nc_"$i".csv then unsparsify then sort -n id then fill-down -f code then cat -n -g code "$folder"/rawdata/tmp_classe_nc_"$i"_join.csv >"$folder"/rawdata/tmp_classe_nc_"$i"_out.csv
+done
+
+mlr --csv cat "$folder"/rawdata/tmp_classe_nc_*_out.csv >"$folder"/rawdata/numeroCasiInizio.csv
+
+cp "$folder"/rawdata/numeroCasiInizio.csv "$folder"/processing/raw_numeroCasiInizio.csv
+cp "$folder"/rawdata/incidenzaInizio.csv "$folder"/processing/raw_incidenzaInizio.csv
+
+python3 "$folder"/iss_epicentroInizio.py
+
+host=$(hostname)
+if [ $host = "ex-machina.ondata.it" ]; then
+    # commit e push
+
+    . ~/.keychain/$HOSTNAME-sh
+
+    cd "$folder"
+    git -C "$folder" pull
+    git -C "$folder" add .
+    git -C "$folder" commit -am "update"
+    git -C "$folder" push origin master
+fi
