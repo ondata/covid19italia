bb2c1c2a50170f0d08cee7b493b9dadd54ccf16c
Andrea Borruso <aborruso@gmail.com>
2020-07-20 19:36:16 +0200

    close #54

--- webservices/regioneLombardia/regioneLombardia.sh
+++ webservices/regioneLombardia/regioneLombardia.sh
@@ -7,39 +7,50 @@
 
 set -x
 
+debugMode="off"
+
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/processing
 mkdir -p "$folder"/processing/INCR_DATE_TAMP_RL_v2
+mkdir -p "$folder"/processing/INCR_DATE_TAMP_RL
 
-while IFS=$'\t' read -r nome formato url; do
-    curl -kL "$url" >"$folder"/rawdata/"$nome"."$formato"
+dos2unix "$folder"/risorse
+
+while IFS=$'\t' read -r nome formato url attivo; do
+    if [[ "$attivo" == "1" ]]; then
+        curl -kL "$url" >"$folder"/rawdata/"$nome"."$formato"
+    fi
 done <"$folder"/risorse
 
 # ogr2ogr -f CSV -lco GEOMETRY=AS_XY "$folder"/processing/ta_covid19_comuni_time.csv "$folder"/rawdata/ta_covid19_comuni_time.geojson
 
-ogr2ogr -f CSV "$folder"/processing/COMUNI_COVID19.csv "$folder"/rawdata/COMUNI_COVID19.geojson
+# ogr2ogr -f CSV "$folder"/processing/COMUNI_COVID19.csv "$folder"/rawdata/COMUNI_COVID19.geojson
 
 ogr2ogr -f CSV "$folder"/processing/PROVINCE_COVID19.csv "$folder"/rawdata/PROVINCE_COVID19.geojson
 
-<"$folder"/rawdata/INCR_DATE_PRV_TAMP_RL_v2.json jq '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_PRV_TAMP_RL_v2.csv
+jq <"$folder"/rawdata/INCR_DATE_PRV_TAMP_RL_v2.json '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_PRV_TAMP_RL_v2.csv
 
 mkdir -p "$folder"/processing/INCR_DATE_PRV_TAMP_RL_v2
 
 date=$(date '+%Y-%m-%d')
 
-<"$folder"/rawdata/INCR_DATE_PRV_TAMP_RL_v2.json jq '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_PRV_TAMP_RL_v2/"$date"-INCR_DATE_PRV_TAMP_RL_v2.csv
+jq <"$folder"/rawdata/INCR_DATE_PRV_TAMP_RL_v2.json '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_PRV_TAMP_RL_v2/"$date"-INCR_DATE_PRV_TAMP_RL_v2.csv
 
-<"$folder"/rawdata/INCR_DATE_TAMP_RL_v2.json jq '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_TAMP_RL_v2/"$date"-INCR_DATE_TAMP_RL_v2.csv
+#jq <"$folder"/rawdata/INCR_DATE_TAMP_RL_v2.json '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_TAMP_RL_v2/"$date"-INCR_DATE_TAMP_RL_v2.csv
 
+jq <"$folder"/rawdata/INCR_DATE_TAMP_RL.json '.features[].attributes' | mlr --j2c cat >"$folder"/processing/INCR_DATE_TAMP_RL/"$date"-INCR_DATE_TAMP_RL.csv
 
-# commit e push
+host=$(hostname)
+if [[ "$host" == "ex-machina.ondata.it" ]] && [[ "$debugMode" == "off" ]]; then
+    # commit e push
+    . ~/.keychain/$HOSTNAME-sh
 
-. ~/.keychain/$HOSTNAME-sh
+    cd "$folder"
+    git -C "$folder" pull
+    git -C "$folder" add .
+    git -C "$folder" commit -am "update"
+    git -C "$folder" push origin master
 
-cd "$folder"
-git -C "$folder" pull
-git -C "$folder" add .
-git -C "$folder" commit -am "update"
-git -C "$folder" push origin master
+fi
