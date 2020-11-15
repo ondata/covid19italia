f15f5141eccc642e97f57d6fa745e3c30d7c819c
Andrea Borruso <aborruso@gmail.com>
2020-11-15 18:18:52 +0100

    aggiungi metadato fonte

--- webservices/agenas/agenas.sh
+++ webservices/agenas/agenas.sh
@@ -19,24 +19,29 @@ code=$(curl -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
 
 # se il sito è raggiungibile scarica i dati
 if [ $code -eq 200 ]; then
-  curl -kL "https://www.agenas.gov.it/covid19/web/index.php?r=site%2Ftab1" | vd -f html +:table_0:: -b -o "$folder"/rawdata/positivi-e-ricoverati.csv
-  curl -kL "https://www.agenas.gov.it/covid19/web/index.php?r=site%2Ftab2" | vd -f html +:table_0:: -b -o "$folder"/rawdata/postiletto-e-ricoverati-areaNonCritica.csv
+  curl -kL "https://www.agenas.gov.it/covid19/web/index.php?r=site%2Ftab1" >"$folder"/rawdata/tab1.html
+  fonte1=$(<"$folder"/rawdata/tab1.html scrape -be ".text-decoration-none" | xq -r '.html.body.p."#text"')
+  vd <"$folder"/rawdata/tab1.html -f html +:table_0:: -b -o "$folder"/rawdata/positivi-e-ricoverati.csv
+
+  curl -kL "https://www.agenas.gov.it/covid19/web/index.php?r=site%2Ftab2" >"$folder"/rawdata/tab2.html
+  fonte2=$(<"$folder"/rawdata/tab2.html scrape -be ".text-decoration-none" | xq -r '.html.body.p."#text"')
+  vd <"$folder"/rawdata/tab2.html -f html +:table_0:: -b -o "$folder"/rawdata/postiletto-e-ricoverati-areaNonCritica.csv
 fi
 
 date=$(date '+%Y-%m-%d')
 
 # rimuovi il separatore delle migliaia
-mlr  -I --csv put -S '
+mlr -I --csv put -S '
   for (k in $*) {
     $[k] = gsub($[k], "\.([0-9])", "\1");
   }
-' "$folder"/rawdata/positivi-e-ricoverati.csv
+' then put '$fonte="'"$fonte1"'"' "$folder"/rawdata/positivi-e-ricoverati.csv
 
 mlr -I --csv put -S '
   for (k in $*) {
     $[k] = gsub($[k], "\.([0-9])", "\1");
   }
-' "$folder"/rawdata/postiletto-e-ricoverati-areaNonCritica.csv
+' then put '$fonte="'"$fonte2"'"' "$folder"/rawdata/postiletto-e-ricoverati-areaNonCritica.csv
 
 mv "$folder"/rawdata/positivi-e-ricoverati.csv "$folder"/processing/"$date"_positivi-e-ricoverati.csv
 mv "$folder"/rawdata/postiletto-e-ricoverati-areaNonCritica.csv "$folder"/processing/"$date"_postiletto-e-ricoverati-areaNonCritica.csv
