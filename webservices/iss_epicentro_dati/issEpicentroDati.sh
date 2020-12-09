26ee6b1dc79d74d13a35f63f5ec3051877aeb604
Andrea Borruso <aborruso@gmail.com>
2020-12-09 23:01:41 +0100

    update

--- webservices/iss_epicentro_dati/issEpicentroDati.sh
+++ webservices/iss_epicentro_dati/issEpicentroDati.sh
@@ -19,15 +19,19 @@ if [ $code -eq 200 ]; then
 
   rm "$folder"/rawdata/*.csv
 
+  # leggi lista dei fogli, rimuovendo Contenuto e foglio che contiene spazio nel nome
   in2csv -n "$folder"/rawdata/covid_19-iss.xlsx | grep -vP "( |Contenuto)" >"$folder"/rawdata/listafogli
 
+  # crea un CSV per ogni foglio
   while read p; do
     in2csv -I --sheet "$p" "$folder"/rawdata/covid_19-iss.xlsx >"$folder"/rawdata/"$p".csv
   done <"$folder"/rawdata/listafogli
 
+  # se la cartella processing è vuota copia i CSV da rawdata a processing
   if [ -z "$(ls -A "$folder"/processing)" ]; then
     cp "$folder"/rawdata/*.csv "$folder"/processing
   else
+    # aggiungi nuovi dati ai fogli archiviati
     while read p; do
       mlr --csv cat then uniq -a "$folder"/processing/"$p".csv "$folder"/rawdata/"$p".csv >"$folder"/processing/tmp.csv
       mv "$folder"/processing/tmp.csv "$folder"/processing/"$p".csv
