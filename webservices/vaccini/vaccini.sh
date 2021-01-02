9918943c408f23c5cd15ccdaa1f7121df8d9d45f
Andrea Borruso <aborruso@gmail.com>
2021-01-02 16:57:01 +0100

    correzioni di logica

--- webservices/vaccini/vaccini.sh
+++ webservices/vaccini/vaccini.sh
@@ -26,7 +26,7 @@ if [ $code -eq 200 ]; then
   # scarica microdati su regioni?
   scaricaR="sì"
 
-  if [[ $scaricaR == "no" ]]; then
+  if [[ $scaricaR == "sì" ]]; then
     while IFS=$'\t' read -r nome codice; do
       echo "$nome"
       curl 'https://wabi-europe-north-b-api.analysis.windows.net/public/reports/querydata?synchronous=true' \
@@ -42,7 +42,7 @@ if [ $code -eq 200 ]; then
         -H 'Sec-Fetch-Dest: empty' \
         -H 'Referer: https://app.powerbi.com/' \
         -H 'Accept-Language: en-US,en;q=0.9,it;q=0.8' \
-        --data-binary $'{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"TAB_MASTER_PIVOT","Type":0},{"Name":"t1","Entity":"TAB_REGIONI","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Valore"},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"},{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"REGIONE"},"Name":"TAB_REGIONI.REGIONE"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Attributo"},"Name":"TAB_MASTER_PIVOT.Attributo"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"KEY"},"Name":"TAB_MASTER_PIVOT.KEY"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Categoria Attributo"},"Name":"TAB_MASTER_PIVOT.Categoria Attributo"}],"Where":[{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"REGIONE"}}],"Values":[[{"Literal":{"Value":"'"\'$nome\'"'"}}]]}}}],"GroupBy":[{"SourceRef":{"Source":"t"},"Name":"TAB_MASTER_PIVOT"}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1,2,3,4],"GroupBy":[0]}]},"DataReduction":{"Primary":{"Top":{"Count":1000}}},"Version":1}}}]},"QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}' \
+        --data-binary $'{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"TAB_MASTER_PIVOT","Type":0},{"Name":"t1","Entity":"TAB_REGIONI","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Valore"},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"},{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"REGIONE"},"Name":"TAB_REGIONI.REGIONE"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Attributo"},"Name":"TAB_MASTER_PIVOT.Attributo"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"KEY"},"Name":"TAB_MASTER_PIVOT.KEY"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Categoria Attributo"},"Name":"TAB_MASTER_PIVOT.Categoria Attributo"}],"Where":[{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"REGIONE"}}],"Values":[[{"Literal":{"Value":"'"\'$nome\'"'"}}]]}}}],"GroupBy":[{"SourceRef":{"Source":"t"},"Name":"TAB_MASTER_PIVOT"}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1,2,3,4],"GroupBy":[0]}]},"DataReduction":{"Primary":{"Top":{"Count":30000}}},"Version":1}}}]},"QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}' \
         --compressed >"$folder"/rawdata/datiRegioni/"$codice".json
 
     done <"$folder"/risorse/listaRegioni.tsv
@@ -59,7 +59,7 @@ if [ $code -eq 200 ]; then
       -H 'Sec-Fetch-Dest: empty' \
       -H 'Referer: https://app.powerbi.com/' \
       -H 'Accept-Language: en-US,en;q=0.9,it;q=0.8' \
-      --data-binary $'{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"TAB_MASTER_PIVOT","Type":0},{"Name":"t1","Entity":"TAB_REGIONI","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Valore"},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"},{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"REGIONE"},"Name":"TAB_REGIONI.REGIONE"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Attributo"},"Name":"TAB_MASTER_PIVOT.Attributo"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"KEY"},"Name":"TAB_MASTER_PIVOT.KEY"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Categoria Attributo"},"Name":"TAB_MASTER_PIVOT.Categoria Attributo"}],"Where":[{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"REGIONE"}}],"Values":[[{"Literal":{"Value":"\'Valle d\'\'Aosta\'"}}]]}}}],"GroupBy":[{"SourceRef":{"Source":"t"},"Name":"TAB_MASTER_PIVOT"}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1,2,3,4],"GroupBy":[0]}]},"DataReduction":{"Primary":{"Top":{"Count":1000}}},"Version":1}}}]},"QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}' \
+      --data-binary $'{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"TAB_MASTER_PIVOT","Type":0},{"Name":"t1","Entity":"TAB_REGIONI","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Valore"},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"},{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"REGIONE"},"Name":"TAB_REGIONI.REGIONE"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Attributo"},"Name":"TAB_MASTER_PIVOT.Attributo"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"KEY"},"Name":"TAB_MASTER_PIVOT.KEY"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Categoria Attributo"},"Name":"TAB_MASTER_PIVOT.Categoria Attributo"}],"Where":[{"Condition":{"In":{"Expressions":[{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"REGIONE"}}],"Values":[[{"Literal":{"Value":"\'Valle d\'\'Aosta\'"}}]]}}}],"GroupBy":[{"SourceRef":{"Source":"t"},"Name":"TAB_MASTER_PIVOT"}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1,2,3,4],"GroupBy":[0]}]},"DataReduction":{"Primary":{"Top":{"Count":30000}}},"Version":1}}}]},"QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}' \
       --compressed >"$folder"/rawdata/datiRegioni/02.json
 
   fi
@@ -90,7 +90,7 @@ if [ $code -eq 200 ]; then
     -H 'Connection: keep-alive' \
     -H 'Referer: https://app.powerbi.com/view?r=eyJrIjoiMzg4YmI5NDQtZDM5ZC00ZTIyLTgxN2MtOTBkMWM4MTUyYTg0IiwidCI6ImFmZDBhNzVjLTg2NzEtNGNjZS05MDYxLTJjYTBkOTJlNDIyZiIsImMiOjh9' \
     -H 'Pragma: no-cache' \
-    -H 'Cache-Control: no-cache' --data-raw '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"TAB_MASTER","Type":0},{"Name":"t1","Entity":"TAB_MASTER_PIVOT","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"TML_FASCIA_ETA"},"Name":"TAB_MASTER.TML_FASCIA_ETA"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"Valore"}},"Function":0},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"}],"OrderBy":[{"Direction":1,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"TML_FASCIA_ETA"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1]}]},"DataReduction":{"DataVolume":4,"Primary":{"Window":{"Count":1000}}},"Version":1}}}]},"CacheKey":"{\"Commands\":[{\"SemanticQueryDataShapeCommand\":{\"Query\":{\"Version\":2,\"From\":[{\"Name\":\"t\",\"Entity\":\"TAB_MASTER\",\"Type\":0},{\"Name\":\"t1\",\"Entity\":\"TAB_MASTER_PIVOT\",\"Type\":0}],\"Select\":[{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t\"}},\"Property\":\"TML_FASCIA_ETA\"},\"Name\":\"TAB_MASTER.TML_FASCIA_ETA\"},{\"Aggregation\":{\"Expression\":{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t1\"}},\"Property\":\"Valore\"}},\"Function\":0},\"Name\":\"Sum(TAB_MASTER_PIVOT.Valore)\"}],\"OrderBy\":[{\"Direction\":1,\"Expression\":{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t\"}},\"Property\":\"TML_FASCIA_ETA\"}}}]},\"Binding\":{\"Primary\":{\"Groupings\":[{\"Projections\":[0,1]}]},\"DataReduction\":{\"DataVolume\":4,\"Primary\":{\"Window\":{\"Count\":1000}}},\"Version\":1}}}]}","QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}' | jq . >"$folder"/rawdata/fasceEta.json
+    -H 'Cache-Control: no-cache' --data-raw '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"TAB_MASTER","Type":0},{"Name":"t1","Entity":"TAB_MASTER_PIVOT","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"TML_FASCIA_ETA"},"Name":"TAB_MASTER.TML_FASCIA_ETA"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"Valore"}},"Function":0},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"}],"OrderBy":[{"Direction":1,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"TML_FASCIA_ETA"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1]}]},"DataReduction":{"DataVolume":4,"Primary":{"Window":{"Count":30000}}},"Version":1}}}]},"CacheKey":"{\"Commands\":[{\"SemanticQueryDataShapeCommand\":{\"Query\":{\"Version\":2,\"From\":[{\"Name\":\"t\",\"Entity\":\"TAB_MASTER\",\"Type\":0},{\"Name\":\"t1\",\"Entity\":\"TAB_MASTER_PIVOT\",\"Type\":0}],\"Select\":[{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t\"}},\"Property\":\"TML_FASCIA_ETA\"},\"Name\":\"TAB_MASTER.TML_FASCIA_ETA\"},{\"Aggregation\":{\"Expression\":{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t1\"}},\"Property\":\"Valore\"}},\"Function\":0},\"Name\":\"Sum(TAB_MASTER_PIVOT.Valore)\"}],\"OrderBy\":[{\"Direction\":1,\"Expression\":{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t\"}},\"Property\":\"TML_FASCIA_ETA\"}}}]},\"Binding\":{\"Primary\":{\"Groupings\":[{\"Projections\":[0,1]}]},\"DataReduction\":{\"DataVolume\":4,\"Primary\":{\"Window\":{\"Count\":30000}}},\"Version\":1}}}]}","QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}' | jq . >"$folder"/rawdata/fasceEta.json
 
   jq <"$folder"/rawdata/fasceEta.json '.results[0].result.data.dsr.DS[0].PH[0].DM0[]' | mlr --j2c unsparsify then cut -r -f "C:" >"$folder"/rawdata/fasceEta.csv
 
@@ -105,26 +105,41 @@ if [ $code -eq 200 ]; then
     -H 'Connection: keep-alive' \
     -H 'Referer: https://app.powerbi.com/view?r=eyJrIjoiMzg4YmI5NDQtZDM5ZC00ZTIyLTgxN2MtOTBkMWM4MTUyYTg0IiwidCI6ImFmZDBhNzVjLTg2NzEtNGNjZS05MDYxLTJjYTBkOTJlNDIyZiIsImMiOjh9' \
     -H 'Pragma: no-cache' \
-    -H 'Cache-Control: no-cache' --data-raw '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t2","Entity":"TAB_MASTER_PIVOT","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Categoria Attributo"},"Name":"TAB_MASTER_PIVOT.Categoria Attributo"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Valore"}},"Function":0},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"}],"OrderBy":[{"Direction":1,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Categoria Attributo"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1]}]},"DataReduction":{"DataVolume":4,"Primary":{"Window":{"Count":1000}}},"Version":1}}}]},"CacheKey":"{\"Commands\":[{\"SemanticQueryDataShapeCommand\":{\"Query\":{\"Version\":2,\"From\":[{\"Name\":\"t2\",\"Entity\":\"TAB_MASTER_PIVOT\",\"Type\":0}],\"Select\":[{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t2\"}},\"Property\":\"Categoria Attributo\"},\"Name\":\"TAB_MASTER_PIVOT.Categoria Attributo\"},{\"Aggregation\":{\"Expression\":{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t2\"}},\"Property\":\"Valore\"}},\"Function\":0},\"Name\":\"Sum(TAB_MASTER_PIVOT.Valore)\"}],\"OrderBy\":[{\"Direction\":1,\"Expression\":{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t2\"}},\"Property\":\"Categoria Attributo\"}}}]},\"Binding\":{\"Primary\":{\"Groupings\":[{\"Projections\":[0,1]}]},\"DataReduction\":{\"DataVolume\":4,\"Primary\":{\"Window\":{\"Count\":1000}}},\"Version\":1}}}]}","QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}' | jq . >"$folder"/rawdata/categoria.json
+    -H 'Cache-Control: no-cache' --data-raw '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t2","Entity":"TAB_MASTER_PIVOT","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Categoria Attributo"},"Name":"TAB_MASTER_PIVOT.Categoria Attributo"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Valore"}},"Function":0},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"}],"OrderBy":[{"Direction":1,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Categoria Attributo"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1]}]},"DataReduction":{"DataVolume":4,"Primary":{"Window":{"Count":30000}}},"Version":1}}}]},"CacheKey":"{\"Commands\":[{\"SemanticQueryDataShapeCommand\":{\"Query\":{\"Version\":2,\"From\":[{\"Name\":\"t2\",\"Entity\":\"TAB_MASTER_PIVOT\",\"Type\":0}],\"Select\":[{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t2\"}},\"Property\":\"Categoria Attributo\"},\"Name\":\"TAB_MASTER_PIVOT.Categoria Attributo\"},{\"Aggregation\":{\"Expression\":{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t2\"}},\"Property\":\"Valore\"}},\"Function\":0},\"Name\":\"Sum(TAB_MASTER_PIVOT.Valore)\"}],\"OrderBy\":[{\"Direction\":1,\"Expression\":{\"Column\":{\"Expression\":{\"SourceRef\":{\"Source\":\"t2\"}},\"Property\":\"Categoria Attributo\"}}}]},\"Binding\":{\"Primary\":{\"Groupings\":[{\"Projections\":[0,1]}]},\"DataReduction\":{\"DataVolume\":4,\"Primary\":{\"Window\":{\"Count\":30000}}},\"Version\":1}}}]}","QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}' | jq . >"$folder"/rawdata/categoria.json
 
   jq <"$folder"/rawdata/categoria.json '.results[0].result.data.dsr.DS[0].PH[0].DM0[]' | mlr --j2c unsparsify then cut -r -f "C:" >"$folder"/rawdata/categoria.csv
 
   # pulizia
 
+  # somministrazioni
   mlr --csv label regione,somministrazioni,percentuale,dosiConsegnate then put '$percentuale=($percentuale*100)' then put -S '$aggiornamento="'"$dataOraAggiornamento"'"' "$folder"/rawdata/somministrazioni.csv >"$folder"/processing/latest_somministrazioni.csv
 
-  cat "$folder"/processing/latest_somministrazioni.csv >>"$folder"/processing/somministrazioni.csv
-  mlr -I --csv uniq -a "$folder"/processing/somministrazioni.csv
+  if [ ! -f "$folder"/processing/somministrazioni.csv ]; then
+    cat "$folder"/processing/latest_somministrazioni.csv >>"$folder"/processing/somministrazioni.csv
+  fi
 
+  mlr --csv uniq -a "$folder"/processing/somministrazioni.csv "$folder"/processing/latest_somministrazioni.csv >"$folder"/processing/tmp.csv
+  mv "$folder"/processing/tmp.csv "$folder"/processing/somministrazioni.csv
+
+  # fasceEta
   mlr --csv label fascia,vaccinazioni then put -S '$aggiornamento="'"$dataOraAggiornamento"'"' "$folder"/rawdata/fasceEta.csv >"$folder"/processing/latest_fasceEta.csv
 
-  cat "$folder"/processing/latest_fasceEta.csv >>"$folder"/processing/fasceEta.csv
-  mlr -I --csv uniq -a "$folder"/processing/fasceEta.csv
+  if [ ! -f "$folder"/processing/fasceEta.csv ]; then
+    cat "$folder"/processing/latest_fasceEta.csv >>"$folder"/processing/fasceEta.csv
+  fi
 
+  mlr --csv uniq -a "$folder"/processing/fasceEta.csv "$folder"/processing/latest_fasceEta.csv >"$folder"/processing/tmp.csv
+  mv "$folder"/processing/tmp.csv "$folder"/processing/fasceEta.csv
+
+  # categoria
   mlr --csv label categoria,vaccinazioni then put -S '$aggiornamento="'"$dataOraAggiornamento"'"' "$folder"/rawdata/categoria.csv >"$folder"/processing/latest_categoria.csv
 
-  cat "$folder"/processing/latest_categoria.csv >>"$folder"/processing/categoria.csv
-  mlr -I --csv uniq -a "$folder"/processing/categoria.csv
+  if [ ! -f "$folder"/processing/categoria.csv ]; then
+    cat "$folder"/processing/latest_categoria.csv >>"$folder"/processing/categoria.csv
+  fi
+
+  mlr --csv uniq -a "$folder"/processing/categoria.csv "$folder"/processing/latest_categoria.csv >"$folder"/processing/tmp.csv
+  mv "$folder"/processing/tmp.csv "$folder"/processing/categoria.csv
 
   date=$(date '+%Y-%m-%d')
 
@@ -159,10 +174,14 @@ if [ $code -eq 200 ]; then
 
   jq -s '.[0] * .[1]' "$folder"/rawdata/maschi.json "$folder"/rawdata/femmine.json >"$folder"/rawdata/sesso.json
 
-  mlr --j2c cat "$folder"/rawdata/sesso.json then put -S '$aggiornamento="'"$dataOraAggiornamento"'"' >"$folder"/processing/latest_sesso.csv
+  mlr --j2c cat then put -S '$aggiornamento="'"$dataOraAggiornamento"'"' "$folder"/rawdata/sesso.json >"$folder"/processing/latest_sesso.csv
 
-  cat "$folder"/processing/latest_sesso.csv >>"$folder"/processing/latest_sesso.csv
+  if [ ! -f "$folder"/processing/fasceEta.csv]; then
+    cat "$folder"/processing/latest_sesso.csv >>"$folder"/processing/sesso.csv
+  fi
 
-  mlr -I --csv uniq -a "$folder"/processing/latest_sesso.csv
+  mlr --csv uniq -a "$folder"/processing/sesso.csv "$folder"/processing/latest_sesso.csv >"$folder"/processing/tmp.csv
+
+  mv "$folder"/processing/tmp.csv "$folder"/processing/sesso.csv
 
 fi
