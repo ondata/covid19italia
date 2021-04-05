56ad4ee6890168db2c1b6b8965fbff14d5bef3b4
Andrea Borruso <aborruso@gmail.com>
2021-04-05 12:34:50 +0200

    update

--- webservices/agenas/agenas_date.sh
+++ webservices/agenas/agenas_date.sh
@@ -7,14 +7,17 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/processing
 
-if [ -f "$folder"/processing/positivi-e-ricoverati.csv ]; then
+if [[ $(hostname) == "DESKTOP-7NVNDNF" ]]; then
+  dconvname="dconv"
+else
+  dconvname="dateutils.dconv"
+fi
 
-  mlr --csv put '$data=system("echo \"".regextract_or_else($fonte,"[0-9]{1,} .+[0-9]{4}","")."\" | dconv --from-locale it_IT  -i \"%d %B %Y\"")' "$folder"/processing/positivi-e-ricoverati.csv >"$folder"/processing/positivi-e-ricoverati_date.csv
+if [ -f "$folder"/processing/positivi-e-ricoverati.csv ]; then
+  mlr --csv put '$data=system("echo \"".regextract_or_else($fonte,"[0-9]{1,} .+[0-9]{4}","")."\" | '"$dconvname"' --from-locale it_IT  -i \"%d %B %Y\"")' "$folder"/processing/positivi-e-ricoverati.csv >"$folder"/processing/positivi-e-ricoverati_date.csv
 fi
 
 if [ -f "$folder"/processing/postiletto-e-ricoverati-areaNonCritica.csv ]; then
-
-  mlr --csv put '$data=system("echo \"".regextract_or_else($fonte,"[0-9]{1,} .+[0-9]{4}","")."\" | dconv --from-locale it_IT  -i \"%d %B %Y\"")' "$folder"/processing/postiletto-e-ricoverati-areaNonCritica.csv >"$folder"/processing/postiletto-e-ricoverati-areaNonCritica_date.csv
+  mlr --csv put '$data=system("echo \"".regextract_or_else($fonte,"[0-9]{1,} .+[0-9]{4}","")."\" | '"$dconvname"' --from-locale it_IT  -i \"%d %B %Y\"")' "$folder"/processing/postiletto-e-ricoverati-areaNonCritica.csv >"$folder"/processing/postiletto-e-ricoverati-areaNonCritica_date.csv
 fi
-
 
