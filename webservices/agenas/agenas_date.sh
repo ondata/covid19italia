323a775d130d8cff36f2ec141e67d7dc6cfa2c80
Andrea Borruso <aborruso@gmail.com>
2021-04-05 12:07:29 +0200

    update

--- webservices/agenas/agenas_date.sh
+++ webservices/agenas/agenas_date.sh
@@ -9,6 +9,12 @@ mkdir -p "$folder"/processing
 
 if [ -f "$folder"/processing/positivi-e-ricoverati.csv ]; then
 
-  echo "ok"
-#put '$test=system("echo \"".regextract_or_else($a,"[0-9] .+[0-9]{4}","")."\" | dconv --from-locale it_IT  -i \"%d %B %Y\"")'
+  mlr --csv put '$data=system("echo \"".regextract_or_else($fonte,"[0-9]{1,} .+[0-9]{4}","")."\" | dconv --from-locale it_IT  -i \"%d %B %Y\"")' "$folder"/processing/positivi-e-ricoverati.csv >"$folder"/processing/positivi-e-ricoverati_date.csv
 fi
+
+if [ -f "$folder"/processing/postiletto-e-ricoverati-areaNonCritica.csv ]; then
+
+  mlr --csv put '$data=system("echo \"".regextract_or_else($fonte,"[0-9]{1,} .+[0-9]{4}","")."\" | dconv --from-locale it_IT  -i \"%d %B %Y\"")' "$folder"/processing/postiletto-e-ricoverati-areaNonCritica.csv >"$folder"/processing/postiletto-e-ricoverati-areaNonCritica_date.csv
+fi
+
+
