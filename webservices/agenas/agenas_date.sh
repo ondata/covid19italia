4732cf04174d7b1d49a4c4d3a0d6fb699d986d91
Andrea Borruso <aborruso@gmail.com>
2021-04-05 13:22:30 +0200

    aggiunto calcolo soglia al 30% e codici nuts

--- webservices/agenas/agenas_date.sh
+++ webservices/agenas/agenas_date.sh
@@ -19,5 +19,7 @@ fi
 
 if [ -f "$folder"/processing/postiletto-e-ricoverati-areaNonCritica.csv ]; then
   mlr --csv put '$data=system("echo \"".regextract_or_else($fonte,"[0-9]{1,} .+[0-9]{4}","")."\" | '"$dconvname"' --from-locale it_IT  -i \"%d %B %Y\"")' "$folder"/processing/postiletto-e-ricoverati-areaNonCritica.csv >"$folder"/processing/postiletto-e-ricoverati-areaNonCritica_date.csv
+  mlr --csv join -j Regioni -f "$folder"/processing/postiletto-e-ricoverati-areaNonCritica_date.csv then unsparsify then sort -f data,Regioni then put '$soglia30=${Ricoverati in Terapia intensiva}/${PL in Terapia Intensiva}*100' "$folder"/risorse/nomiCodiciRegioni.csv >"$folder"/tmp.csv
+  mv "$folder"/tmp.csv "$folder"/processing/postiletto-e-ricoverati-areaNonCritica_date.csv
 fi
 
