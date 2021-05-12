8fdd4d6cebc8ca5c6a9dec8448541c289a47d234
Andrea Borruso <aborruso@gmail.com>
2021-05-12 08:39:07 +0200

    primo test per Paolo

---
+++ docs/terapiaintensiva/js/config.js
@@ -0,0 +1,33 @@
+// carica definizione grafico Nazionale
+const specN = "./spec/terapieIntensiveN.json";
+// renderizza grafico Nazionale
+vegaEmbed('#visN', specN, {
+  theme: "fivethirtyeight",
+  timeFormatLocale: {
+    "dateTime": "%A %e %B %Y, %X",
+    "date": "%d/%m/%Y",
+    "time": "%H:%M:%S",
+    "periods": ["AM", "PM"],
+    "days": ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"],
+    "shortDays": ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"],
+    "months": ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+    "shortMonths": ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"]
+  }
+})
+
+// carica definizione grafico Regionale
+const specR = "./spec/terapieIntensiveR.json";
+// renderizza grafico Regionale
+vegaEmbed('#visR', specR, {
+  theme: "fivethirtyeight",
+  timeFormatLocale: {
+    "dateTime": "%A %e %B %Y, %X",
+    "date": "%d/%m/%Y",
+    "time": "%H:%M:%S",
+    "periods": ["AM", "PM"],
+    "days": ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"],
+    "shortDays": ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"],
+    "months": ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+    "shortMonths": ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"]
+  }
+})
