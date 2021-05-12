000676378257dba82c0e4b6025a263487f73f108
Andrea Borruso <aborruso@gmail.com>
2021-05-12 08:38:53 +0200

    aggiornamento dati TI

---
+++ elaborazioni/dpc_covid19/terapiaIntensiva/terapiaIntensiva.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### requisiti ###
+# sqlite3 https://www.sqlite.org/index.html
+# miller https://github.com/johnkerl/miller
+# moreutils https://joeyh.name/code/moreutils/
+### requisiti ###
+
+### output ###
+# https://bl.ocks.org/aborruso/raw/28374f1d59a5d9880c4c76dc66865cd8/
+### output ###
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$folder"/rawdata
+mkdir -p "$folder"/processing
+mkdir -p "$folder"/risorse
+
+# dati nazione
+URLN="https://download.data.world/s/67bofmmi7jubq2d6cezbt7qxx4iwba"
+# dati regioni
+URLR="https://download.data.world/s/mj726nk3buk65abrmkvpgcs5mbxsul"
+
+curl -kL  "$URLN" >"$folder"/processing/terapiaIntensivaNaz.csv
+curl -kL  "$URLR" >"$folder"/processing/terapiaIntensivaReg.csv
+
