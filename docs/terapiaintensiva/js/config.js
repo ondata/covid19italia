ddf443cb31f30b4da2ff34e500058b8675046e83
Andrea Borruso <aborruso@gmail.com>
2021-05-14 14:38:24 +0200

    pulizia

--- docs/terapiaintensiva/js/config.js
+++ docs/terapiaintensiva/js/config.js
@@ -12,7 +12,8 @@ vegaEmbed('#visN', specN, {
     "shortDays": ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"],
     "months": ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
     "shortMonths": ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"]
-  }
+  },
+  actions: false
 })
 
 // carica definizione grafico Regionale
@@ -29,7 +30,8 @@ vegaEmbed('#visR', specR, {
     "shortDays": ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"],
     "months": ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
     "shortMonths": ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"]
-  }
+  },
+  actions: false
 }).then(res => {
   view = res.view;
   console.log(view.data('source_0'));
