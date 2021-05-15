906e1f15d0aafa9becc7d5470928e77a77e2a083
Andrea Borruso <aborruso@gmail.com>
2021-05-15 09:07:20 +0200

    aggiunti dati soglia

--- elaborazioni/dpc_covid19/terapiaIntensiva/terapiaIntensiva.sh
+++ elaborazioni/dpc_covid19/terapiaIntensiva/terapiaIntensiva.sh
@@ -25,11 +25,16 @@ mkdir -p "$folder"/risorse
 URLN="https://download.data.world/s/67bofmmi7jubq2d6cezbt7qxx4iwba"
 # dati regioni
 URLR="https://download.data.world/s/mj726nk3buk65abrmkvpgcs5mbxsul"
+# dati regioni con soglia
+URLRS="https://download.data.world/s/oherutj4dwgwrg6uofmr7i6psjburd"
 
 curl -kL  "$URLN" >"$folder"/processing/terapiaIntensivaNaz.csv
 curl -kL  "$URLR" >"$folder"/processing/terapiaIntensivaReg.csv
+curl -kL  "$URLRS" >"$folder"/processing/terapiaIntensivaRegSoglia.csv
 
 mlr -I --csv sort -f data,denominazione_regione "$folder"/processing/terapiaIntensivaReg.csv
+mlr -I --csv sort -f data,denominazione_regione "$folder"/processing/terapiaIntensivaRegSoglia.csv
 
 dos2unix "$folder"/processing/terapiaIntensivaNaz.csv
 dos2unix "$folder"/processing/terapiaIntensivaReg.csv
+dos2unix "$folder"/processing/terapiaIntensivaRegSoglia.csv
