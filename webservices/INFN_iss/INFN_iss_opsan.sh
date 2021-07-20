977e5766f92c9631485987103f35b4734d1c9f0d
Andrea Borruso <aborruso@gmail.com>
2021-07-20 13:00:35 +0200

    update

---
+++ webservices/INFN_iss/INFN_iss_opsan.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$folder"/processing
+mkdir -p "$folder"/rawdata
+
+dataset="iss_opsan_italia"
+
+# sintomatici operatori
+find "$folder"/processing/"$dataset"/ -name "*.json" | while read line; do
+  nome=$(basename "$line" ".json")
+  <"$line" jq '.[0]|[.]|map([.x, .y]| transpose|.[]|{x:.[0],y:.[1]})'  | mlr --j2c label date,valore >"$folder"/processing/"$dataset"/operatori_"$nome".csv
+done
+
+# sintomatici altri
+find "$folder"/processing/"$dataset"/ -name "*.json" | while read line; do
+  nome=$(basename "$line" ".json")
+  <"$line" jq '.[1]|[.]|map([.x, .y]| transpose|.[]|{x:.[0],y:.[1]})'  | mlr --j2c label date,valore >"$folder"/processing/"$dataset"/altri_"$nome".csv
+done
+
+# cancella file csv vuoti
+find "$folder"/processing/"$dataset"/ -name "*.csv" -size 0 -delete
+
+# merge dei CSV
+
+mlr --csv put '$r=FILENAME;$r=gsub($r,"(.+_opsan_|\.csv)","");$regione=regextract($r,"^(.+_giulia|.+_romagna|.+_daosta|.+_adige|.._[a-z]+|[a-z]+)_");$tipo=sub($r,$regione,"");$regione=sub($regione,"_","")' then cut -x -f r then sort -f regione,tipo,date "$folder"/processing/iss_opsan_italia/operatori*.csv >"$folder"/processing/iss_sintomatici_opsan.csv
+
+mlr --csv put '$r=FILENAME;$r=gsub($r,"(.+_opsan_|\.csv)","");$regione=regextract($r,"^(.+_giulia|.+_romagna|.+_daosta|.+_adige|.._[a-z]+|[a-z]+)_");$tipo=sub($r,$regione,"");$regione=sub($regione,"_","")' then cut -x -f r then sort -f regione,tipo,date "$folder"/processing/iss_opsan_italia/altri*.csv >"$folder"/processing/iss_sintomatici_altri.csv
