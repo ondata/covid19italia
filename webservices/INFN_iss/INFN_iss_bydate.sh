3356fba4c657e39025754d730bd84fdbc943c4b6
Andrea Borruso <aborruso@gmail.com>
2021-07-20 15:21:50 +0200

    creata version wide come da #108

--- webservices/INFN_iss/INFN_iss_bydate.sh
+++ webservices/INFN_iss/INFN_iss_bydate.sh
@@ -16,5 +16,8 @@ find "$folder"/processing/iss_bydate_italia/regioni -name "*.json" | while read 
   <"$line" jq '.[1]|[.]|map([.x, .y]| transpose|.[]|{x:.[0],y:.[1]})'  | mlr --j2c label date,nuoviCasiGiornalieri >"$folder"/processing/iss_bydate_italia/regioni/"$nome".csv
 done
 
-# file di insieme
-#mlr --csv put '$fn=FILENAME;$fn=sub($fn,".+/","");$fn=sub($fn,"iss_bydate_","");$fn=sub($fn,".csv$","");$regione=regextract($fn,"^(.+_giulia|.+_romagna|.+_daosta|.+_adige|.._[a-z]+|[a-z]+)_");$tipo=sub($fn,$regione,"");$regione=sub($regione,"_","")' then cut -x -f fn then sort -f regione,tipo,date "$folder"/processing/iss_bydate_italia/regioni/*.csv >"$folder"/processing/iss_bydate_italia/regioni.csv
+# file di insieme long
+mlr --csv put '$fn=FILENAME;$fn=sub($fn,".+/","");$fn=sub($fn,"iss_bydate_","");$fn=sub($fn,".csv$","");$regione=regextract($fn,"^(.+_giulia|.+_romagna|.+_daosta|.+_adige|.._[a-z]+|[a-z]+)_");$tipo=sub($fn,$regione,"");$regione=sub($regione,"_","")' then cut -x -f fn then sort -f regione,tipo,date "$folder"/processing/iss_bydate_italia/regioni/*.csv >"$folder"/processing/iss_bydate_italia/regioni.csv
+
+# file di insieme wide
+mlr --csv  reshape -s tipo,nuoviCasiGiornalieri then unsparsify then sort -f regione,date "$folder"/processing/iss_bydate_italia/regioni.csv >"$folder"/processing/iss_bydate_italia/regioni_wide.csv
