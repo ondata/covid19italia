db5f293391943ff87c934c5f371711a2e7c8a8a8
Andrea Borruso <aborruso@gmail.com>
2021-07-20 09:15:25 +0200

    aggiunti CSV RT

---
+++ webservices/INFN_iss/INFN_iss_rt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$folder"/processing
+mkdir -p "$folder"/rawdata
+
+# RT regioni
+find "$folder"/processing/iss_rt/regioni -name "*.json" | while read line; do
+  nome=$(basename "$line" ".json")
+  <"$line" jq '.[0]|[.]|map([.x, .y]| transpose|.[]|{x:.[0],y:.[1]})'  | mlr --j2c label date,rt >"$folder"/processing/iss_rt/regioni/"$nome".csv
+done
+
+
+# RT province
+find "$folder"/processing/iss_rt/province -name "*.json" | while read line; do
+  nome=$(basename "$line" ".json")
+  <"$line" jq '.[0]|[.]|map([.x, .y]| transpose|.[]|{x:.[0],y:.[1]})'  | mlr --j2c label date,rt >"$folder"/processing/iss_rt/province/"$nome".csv
+done
