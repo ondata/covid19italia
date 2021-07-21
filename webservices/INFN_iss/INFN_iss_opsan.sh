28829b8f5b8fd86806de3e4a6f29fd016e75a184
Andrea Borruso <aborruso@gmail.com>
2021-07-21 17:01:51 +0200

    creazione file di insieme

--- webservices/INFN_iss/INFN_iss_opsan.sh
+++ webservices/INFN_iss/INFN_iss_opsan.sh
@@ -30,5 +30,13 @@ find "$folder"/processing/"$dataset"/ -name "*.csv" -size 0 -delete
 # merge dei CSV
 
 mlr --csv put '$r=FILENAME;$r=gsub($r,"(.+_opsan_|\.csv)","");$regione=regextract($r,"^(.+_giulia|.+_romagna|.+_daosta|.+_adige|.._[a-z]+|[a-z]+)_");$tipo=sub($r,$regione,"");$regione=sub($regione,"_","")' then cut -x -f r then sort -f regione,tipo,date "$folder"/processing/iss_opsan_italia/operatori*.csv >"$folder"/processing/iss_sintomatici_opsan.csv
+# creazione versione wide
+mlr --csv reshape -s tipo,valore then unsparsify then sort -f regione,date "$folder"/processing/iss_sintomatici_opsan.csv >"$folder"/processing/iss_sintomatici_opsan_wide.csv
+
 
 mlr --csv put '$r=FILENAME;$r=gsub($r,"(.+_opsan_|\.csv)","");$regione=regextract($r,"^(.+_giulia|.+_romagna|.+_daosta|.+_adige|.._[a-z]+|[a-z]+)_");$tipo=sub($r,$regione,"");$regione=sub($regione,"_","")' then cut -x -f r then sort -f regione,tipo,date "$folder"/processing/iss_opsan_italia/altri*.csv >"$folder"/processing/iss_sintomatici_altri.csv
+# creazione versione wide
+mlr --csv reshape -s tipo,valore then unsparsify then sort -f regione,date "$folder"/processing/iss_sintomatici_altri.csv >"$folder"/processing/iss_sintomatici_altri_wide.csv
+
+# cancella i file CSV singoli
+find "$folder"/processing/iss_opsan_italia -name "*.csv" -delete
