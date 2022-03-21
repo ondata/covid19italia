0e128099d274073d65a7edd957a83c2ca71d49e1
Andrea Borruso <aborruso@gmail.com>
2022-03-21 10:05:20 +0100

    Gestione data di fine sconosciuta

--- webservices/COVID-19Aree/COVID-19Aree.sh
+++ webservices/COVID-19Aree/COVID-19Aree.sh
@@ -115,7 +115,7 @@ from tmp"
         {$zona="bianca"}
       else
         {$zona="NA"}' then \
-    put -S '$datasetIniISO = strftime(strptime($datasetIni, "%d/%m/%Y"),"%Y-%m-%d");$datasetFinISO = strftime(strptime($datasetFin, "%d/%m/%Y"),"%Y-%m-%d")' then \
+    put -S '$datasetIniISO = strftime(strptime($datasetIni, "%d/%m/%Y"),"%Y-%m-%d");$datasetFinISO = if($datasetFin=~"[0-9]{2}/[0-9]{2}/[0-9]{4}"){strftime(strptime($datasetFin, "%d/%m/%Y"),"%Y-%m-%d")}else{""}' then \
     sort -f datasetIniISO,nomeTesto then put -S '$versionID=sub($versionID,"^0+","")' "$folder"/processing/areeStorico.csv
 
   # aggiungi codici NUTS
